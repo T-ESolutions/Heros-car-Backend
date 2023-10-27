260dca1bf0794ee4e94dd59714922109b8b67e3f
Andrew Akram Albert <andrewalbert93501@gmail.com>
2023-10-27 23:44:56 +0200

    creat trip in user

--- app/Http/Controllers/Eloquent/V1/User/TripRepository.php
+++ app/Http/Controllers/Eloquent/V1/User/TripRepository.php
@@ -95,7 +95,7 @@ class TripRepository implements TripRepositoryInterface
             'end_address_ar'        => $request->end_address_ar,
             'end_address_en'        => $request->end_address_en,
         ]);
-        return $tripRequest->id;
+        return TripRequest::whereId($tripRequest->id)->with(['driver','driverCar'])->first();
     }
 
     public function searchTrip($request)
